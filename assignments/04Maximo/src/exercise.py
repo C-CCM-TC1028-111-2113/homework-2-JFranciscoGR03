3be4158ece3fc025b92ecd63ae77ed6108387e5b
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-19 21:01:08 -0500

    Solved!

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,6 +1,21 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    numero1= int(input("Ingresa el primer número: "))
+    numero2= int(input("Ingresa el segundo número: "))
+    numero3= int(input("Ingresa el tercer número: "))
+
+    if numero1<numero2<numero3:
+        print(numero3)
+    elif numero2<numero1<numero3:
+        print(numero3)
+    elif numero1<numero3<numero2:
+        print(numero2)
+    elif numero3<numero1<numero2:
+        print(numero2)
+    elif numero2<numero2<numero1:
+        print(numero1)
+    else:
+        print(numero1)
 
 
 if __name__=='__main__':
