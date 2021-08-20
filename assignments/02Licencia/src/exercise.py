b05fba2430ecac951778b6137c8df01301731b30
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-19 20:15:48 -0500

    Solved!

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,7 +1,15 @@
 
 def main():
     #Escribe tu código debajo de esta línea
-    pass
+    edad= int(input("Ingresa tu edad: "))
+    iden= input("¿Tienes identificación oficial? (s/n): ")
+
+    if edad>=18 and iden=="s":
+        print("Trámite de licencia concedido")
+    elif edad<18 or iden=="n":
+        print("No cumples requisitos")
+    elif edad<0 or iden!="s" or iden!="n":
+        print("Respuesta incorrecta")
 
 
 if __name__ == '__main__':
