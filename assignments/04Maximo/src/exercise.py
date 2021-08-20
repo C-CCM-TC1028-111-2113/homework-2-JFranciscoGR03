6655faa80487667f6cc1ceab1a76f78aeb752d73
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-19 21:15:18 -0500

    Solved!

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,8 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
-    num1= int(input("Ingresa el primer número: "))
-    num2= int(input("Ingresa el segundo número: "))
-    num3= int(input("Ingresa el tercer número: "))
+    num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+    num3 = int(input("Ingresa el tercer número: "))
 
     if num1<num2<num3:
         print(num3)
