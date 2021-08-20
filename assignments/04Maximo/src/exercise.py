feecae142850810d3db6e31fba03e4fbc798e228
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-19 21:12:39 -0500

    Solved!

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,21 +1,21 @@
 def main():
     #escribe tu código abajo de esta línea
-    numero1= int(input("Ingresa el primer número: "))
-    numero2= int(input("Ingresa el segundo número: "))
-    numero3= int(input("Ingresa el tercer número: "))
+    num1= int(input("Ingresa el primer número: "))
+    num2= int(input("Ingresa el segundo número: "))
+    num3= int(input("Ingresa el tercer número: "))
 
-    if numero1<numero2<numero3:
-        print(numero3)
-    elif numero2<numero1<numero3:
-        print(numero3)
-    elif numero1<numero3<numero2:
-        print(numero2)
-    elif numero3<numero1<numero2:
-        print(numero2)
-    elif numero2<numero3<numero1:
-        print(numero1)
-    elif numero3<numero2<numero1:
-        print(numero1)
+    if num1<num2<num3:
+        print(num3)
+    elif num2<num1<num3:
+        print(num3)
+    elif num1<num3<num2:
+        print(num2)
+    elif num3<num1<num2:
+        print(num2)
+    elif num2<num3<num1:
+        print(num1)
+    elif num3<num2<num1:
+        print(num1)
 
 
 if __name__=='__main__':
